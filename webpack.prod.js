c56ee628d8d8a4f75aea2ed813e6d297eec0a81c
Svetlana Terenteva <gubinas83@gmail.com>
2020-12-17 16:39:07 +0300

    feat: add project build scripts

---
+++ webpack.prod.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const common = require("./webpack.common.js");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = merge (common, {
+    mode: `production`,
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js',
+    },
+    module: {
+      rules: [
+        {
+            test: /\.(sass|scss)$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+              },
+             'css-loader', 'postcss-loader', 'sass-loader',
+            ],
+          },
+      ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+        new CopyWebpackPlugin([
+            {from:'src/img',to:'img'},
+        ]),
+        new HtmlWebpackPlugin({
+            hash: true,
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+    ]
+});
