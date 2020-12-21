696f5ee237e050e97e9ae3349df8af505531f881
Svetlana Terenteva <gubinas83@gmail.com>
2020-12-21 15:16:55 +0300

    fix: fix config for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
         browser: true,
         node: true,
     },
-    extends: ['airbnb'],
+    extends: ['airbnb-base'],
     parserOptions: {
         ecmaVersion: 2015,
         sourceType: 'module',
